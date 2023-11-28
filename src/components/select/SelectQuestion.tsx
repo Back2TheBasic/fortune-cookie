33c4ee18a722e599662ff95be3fe1204b4e3247f
soyoonJ <sytkfkd96@gmail.com>
2023-11-29 00:28:19 +0900

    feat: selectQuestion route to result

--- src/components/select/SelectQuestion.tsx
+++ src/components/select/SelectQuestion.tsx
@@ -1,15 +1,27 @@
+import { useRouter } from 'next/navigation';
 import Button from '../Button';
 import Title from '../Title';
 import styles from './SelectQuestion.module.scss';
 
 const SelectQuestion = () => {
+  const router = useRouter();
   const questions: string[] = ['질문1', '질문2', '질문3'];
+
+  const selectQuestion = (question: string) => {
+    router.push('/result');
+  };
 
   return (
     <>
       <Title title="고민 선택" />
       {questions.map((question) => {
-        return <Button key={question} name={question} />;
+        return (
+          <Button
+            onClick={() => selectQuestion(question)}
+            key={question}
+            name={question}
+          />
+        );
       })}
       <button type="button">새로고침</button>
     </>
