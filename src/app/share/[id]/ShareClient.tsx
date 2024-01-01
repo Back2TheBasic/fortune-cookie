4e1c6fa31a84a06b9561e1ecbc18678e74d54b0d
Bong <borobong230@gmail.com>
2024-01-01 18:13:58 +0900

    feat: 공유화면 추가 close #48

---
+++ src/app/share/[id]/ShareClient.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import FortuneCookieShare from '@/components/fortune-cookie-share/FortuneCookieShare';
+
+const ShareClient = () => {
+  return (
+    <div id="share-client">
+      <FortuneCookieShare />
+    </div>
+  );
+};
+
+export default ShareClient;
---
+++ src/app/share/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ShareClient from './ShareClient';
+
+const Share = () => {
+  return <ShareClient />;
+};
+
+export default Share;
