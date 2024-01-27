73376e034d5a63c98e7091e477c2fc649eb1805b
soyoonJ <sytkfkd96@gmail.com>
2024-01-27 23:29:16 +0900

    [chore]: next.config.js rewrites 옵션 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: '/share/:id',
+        destination: '/share/[id]',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
